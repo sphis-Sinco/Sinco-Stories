c851c6ac76b6777bc24aa8ab31fd60c347232c2b
Sphis-Sinco <sinconsistencia@gmail.com>
2025-03-14 17:29:21 -0500

    selections

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -4,6 +4,11 @@ import flixel.FlxState;
 
 class MenuState extends FlxState
 {
+	var selections:Map<String, Dynamic> = [
+		"War of the seven worlds" => () -> new MenuState(),
+		#if sys "Leave" => () -> Sys.exit(0) #end
+	];
+
 	override public function create()
 	{
 		super.create();
