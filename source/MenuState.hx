f81de7d36b7d45aef090cc2285fd6365dc9e7d2b
Sphis-Sinco <sinconsistencia@gmail.com>
2025-03-14 17:39:22 -0500

    selection basics

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -3,6 +3,7 @@ package;
 import flixel.FlxState;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.text.FlxText;
+import sinlib.utilities.TryCatch;
 
 class MenuState extends FlxState
 {
@@ -11,10 +12,31 @@ class MenuState extends FlxState
 		#if sys "Leave" => () -> Sys.exit(0) #end
 	];
 	var selectionText:FlxTypedGroup<FlxText> = new FlxTypedGroup<FlxText>();
+	var selected:Int = 0;
 
 	override public function create()
 	{
 		add(selectionText);
+		updateSelectionText();
+
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+
+	function updateSelectionText()
+	{
+		TryCatch.tryCatch(() ->
+		{
+			for (member in selectionText.members)
+			{
+				member.destroy();
+				selectionText.members.remove(member);
+			}
+		});
 
 		var index:Int = 0;
 		for (text in selections.keys())
@@ -23,16 +45,14 @@ class MenuState extends FlxState
 			var textfield:FlxText = new FlxText(16, 0, 0, text, 16);
 			textfield.y = 16 + (24 * index);
 
+			if (selected == index)
+			{
+				textfield.color = 0xFFFF00;
+			}
+
 			selectionText.add(textfield);
 
 			index++;
 		}
-
-		super.create();
-	}
-
-	override public function update(elapsed:Float)
-	{
-		super.update(elapsed);
 	}
 }
