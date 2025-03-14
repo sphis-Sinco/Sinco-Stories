f647fa08d304fe9183dd7ddb34d31599db237bf2
Sphis-Sinco <sinconsistencia@gmail.com>
2025-03-14 17:32:51 -0500

    visible text selections

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -1,6 +1,8 @@
 package;
 
 import flixel.FlxState;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.text.FlxText;
 
 class MenuState extends FlxState
 {
@@ -8,12 +10,22 @@ class MenuState extends FlxState
 		"War of the seven worlds" => () -> new MenuState(),
 		#if sys "Leave" => () -> Sys.exit(0) #end
 	];
+	var selectionText:FlxTypedGroup<FlxText> = new FlxTypedGroup<FlxText>();
 
 	override public function create()
 	{
+		add(selectionText);
+
+		var index:Int = 0;
 		for (text in selections.keys())
 		{
 			trace(text);
+			var textfield:FlxText = new FlxText(16, 0, 0, text, 16);
+			textfield.y = 16 + (24 * index);
+
+			selectionText.add(textfield);
+
+			index++;
 		}
 
 		super.create();
